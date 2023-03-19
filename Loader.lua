1aeafe62d3d665782b4aa4f157519b9bf3c3b382
zShadowSkilled <103582788+zShadowSkilled1@users.noreply.github.com>
2023-03-19 14:22:09 +0100

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -4,11 +4,14 @@ Register - Good for Tower Of Hell and the anticheats that kick, only recomanded 
 KickHook - Prevent any script/anticheat from kicking you
 IndexHook - Spoof your walkspeed to 0
 BlockRemote - If the anticheat use a remote, then block it
+
+==Others Modes==
+
 Automatic - Let the script choose the best method for the game you're playing
 ]]
 
 local Method = "Automatic"
 
-wait(game:IsLoaded())
+getgenv().Method = Method
 
-local SoapBypass = loadstring(game:HttpGet("https://raw.githubusercontent.com/zShadowSkilled1/SoapBypass/main/SoapBypass.lua"))()getgenv().Method = Method
+local SoapBypass = loadstring(game:HttpGet('https://raw.githubusercontent.com/zShadowSkilled1/SoapBypass/main/SoapBypass.lua'))()
