e8c1f3e8c00a91f3516a19a0242e33ba3401ec1b
zShadowSkilled <103582788+zShadowSkilled1@users.noreply.github.com>
2023-04-02 10:12:32 +0200

    Update Blacklist.lua

--- Blacklist.lua
+++ Blacklist.lua
@@ -1,3 +1,3 @@
-if game:GetService("RbxAnalyticsService"):GetClientId() == "" or game.Players.LocalPlayer.Name == "" then
+if game:GetService("RbxAnalyticsService"):GetClientId() == "" or game.Players.LocalPlayer.Name == "zShadowSkiIled2A" then
 return false
 end
